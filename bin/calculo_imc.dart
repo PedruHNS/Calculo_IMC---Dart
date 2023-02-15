886e1dcdd343175af43db474f2efd71a78bb822a
Pedro Siqueira <pedro.siqueira1999@hotmail.com>
2023-02-15 11:32:28 -0300

    codigo melhorado, com repetição e com alteração no construtor

--- bin/calculo_imc.dart
+++ bin/calculo_imc.dart
@@ -1,20 +1,5 @@
 import 'package:calculo_imc/funcoes.dart';
-import 'package:calculo_imc/classe.dart';
 
 void main() {
-  //!não precisa ser convertido pois já é uma string.
-  final nome = entradaDados(mensagem: "Digite o nome");
-  //!os demais precisam ser convertido pois são string.
-  final idade = int.parse(entradaDados(mensagem: "digite a idade"));
-  final altura = double.parse(entradaDados(mensagem: "digite a altura"));
-  final peso = double.parse(entradaDados(mensagem: "digite o peso"));
-
-  Atleta pessoa = Atleta.nomeado(
-    nome: nome,
-    idade: idade,
-    altura: altura,
-    peso: peso,
-  );
-
-  pessoa.imc();
+  finalizar();
 }
--- lib/funcoes.dart
+++ lib/funcoes.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
+import 'package:calculo_imc/classe.dart';
 
-String entradaDados({required String mensagem}) {
+String input({required String mensagem}) {
   String? input;
   while (input == null) {
     print(mensagem);
@@ -9,4 +10,28 @@ String entradaDados({required String mensagem}) {
   return input;
 }
 
+void imc() {
+  final Atleta pessoa = Atleta.nomeado(
+    nome: input(mensagem: "Digite o nome"),
+    idade: int.parse(input(mensagem: "digite a idade")),
+    altura: double.parse(input(mensagem: "digite a altura")),
+    peso: double.parse(input(mensagem: "digite o peso")),
+  );
 
+  pessoa.imc();
+}
+
+void finalizar() {
+  String sair = input(mensagem: "Deseja finalizar?").toLowerCase();
+  if (sair == "sim") {
+    print("finalizamos");
+    return;
+  }
+  if (sair == "nao") {
+    imc();
+    finalizar();
+    return;
+  }
+  print("não entendi, digite novamente");
+  finalizar();
+}
